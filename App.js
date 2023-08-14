cdd6338c6e77fa8c5e1bf9e0d9634d6b8f9ef36d
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-08-14 16:18:30 -0400

    basis of afc mob

--- App.js
+++ App.js
@@ -1,20 +1,117 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>AFC MOBILE COMING SOON</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.contentWrapper}>
+        <View style={styles.header}>
+          <Image source={require('./assets/AFC-logo.png')} style={styles.logo} />
+          <Button title="Profile" onPress={() => {}} />
+        </View>
+
+        <View style={styles.locationSection}>
+          <Text>Your Location: [Location Here]</Text>
+          <Button title="Update Location" onPress={() => {}} />
+        </View>
+
+        <TextInput style={styles.searchBar} placeholder="Search for a professional..." />
+
+        <FlatList
+          data={[{key: 'Dr. Smith'}, {key: 'Dr. Jane'}, {key: 'Dr. Doe'}]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+
+        <View style={styles.divider} />
+
+
+        <StatusBar style="auto" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>American Family Care </Text>
+          <Text style={styles.footerText}> Built by Alejandro Otaola </Text>
+          <View style={styles.footerButtons}>
+            <Button title="Settings" onPress={() => {}} color="#6c757d" />
+            <Button title="Help" onPress={() => {}} color="#6c757d" />
+          </View>
+        </View>
+      </View>
+   </View>
+);
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
+    width: '100%',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    postion: 'center',
+    width: 80,  // or whatever size you want
+    height: 80, // or whatever size you want
+    resizeMode: 'contain', // or 'cover' or 'stretch'
+
+  },
+  contentWrapper: {
+    height: '85%',
+    width: '80%', // This will constrain the content to 80% of the screen width
+  },
+  header: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  headerText: {
+    backgroundColor: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  locationSection: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  searchBar: {
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+  },
+  item: {
+    backgroundColor: 'white',
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    borderBottomColor: 'blue',
+    borderBottomWidth: 1,
+  },
+  footer: {
+    borderWidth: '1px',
+    borderColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  footerText: {
+    borderWidth: '1px',
+    borderColor: 'white',
+    fontSize: 16,
+    color: '#6c757d',
+  },
+  footerButtons: {
+    borderWidth: '1px',
+    borderColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 150,
   },
 });
